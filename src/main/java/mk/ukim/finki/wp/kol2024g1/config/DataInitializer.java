e1aba8c5ad22c4e59d142d79892d88bff86d458d
David Hristov <113634059+davidhristov59@users.noreply.github.com>
2025-01-12 04:42:09 +0100

    initialized the data with @PostConstruct and @Component

--- src/main/java/mk/ukim/finki/wp/kol2024g1/config/DataInitializer.java
+++ src/main/java/mk/ukim/finki/wp/kol2024g1/config/DataInitializer.java
@@ -1,11 +1,14 @@
 package mk.ukim.finki.wp.kol2024g1.config;
 
+import jakarta.annotation.PostConstruct;
 import mk.ukim.finki.wp.kol2024g1.model.RoomType;
 import mk.ukim.finki.wp.kol2024g1.service.HotelService;
 import mk.ukim.finki.wp.kol2024g1.service.ReservationService;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
+@Component
 public class DataInitializer {
 
     private final HotelService hotelService;
@@ -16,12 +19,12 @@ public class DataInitializer {
         this.reservationService = reservationService;
     }
 
-
     private RoomType randomize(int i) {
         if (i % 2 == 0) return RoomType.SINGLE;
         return RoomType.DOUBLE;
     }
 
+    @PostConstruct
     public void initData() {
         for (int i = 1; i < 6; i++) {
             this.hotelService.create("Hotel: " + i);

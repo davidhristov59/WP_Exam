4ee50bcd28e75e5db7013ee1980d62c49075ebdd
David Hristov <113634059+davidhristov59@users.noreply.github.com>
2025-01-12 04:45:50 +0100

    added exceptions

--- src/main/java/mk/ukim/finki/wp/kol2024g1/model/exceptions/InvalidHotelIdException.java
+++ src/main/java/mk/ukim/finki/wp/kol2024g1/model/exceptions/InvalidHotelIdException.java
@@ -1,4 +1,7 @@
 package mk.ukim.finki.wp.kol2024g1.model.exceptions;
 
 public class InvalidHotelIdException extends RuntimeException {
+    public InvalidHotelIdException(Long id) {
+        super("The hotel with id" + id + " is not found");
+    }
 }
--- src/main/java/mk/ukim/finki/wp/kol2024g1/model/exceptions/InvalidReservationIdException.java
+++ src/main/java/mk/ukim/finki/wp/kol2024g1/model/exceptions/InvalidReservationIdException.java
@@ -1,4 +1,7 @@
 package mk.ukim.finki.wp.kol2024g1.model.exceptions;
 
 public class InvalidReservationIdException extends RuntimeException {
+    public InvalidReservationIdException(Long id) {
+        super("The reservation with id " + id + " is not found");
+    }
 }
